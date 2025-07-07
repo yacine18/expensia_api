c1ed791deee0a1b765a5fe21be56d278b788675f
Yassine Hilali <hilali.yacin@gmail.com>
2025-07-07 15:33:01 +0100

    setup spring boot project and set folder structure

---
+++ src/test/java/com/yassine/expensia/ExpensiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassine.expensia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
